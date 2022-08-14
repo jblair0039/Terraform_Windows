7870e6c72db7d69e815c4a06390a8e9b2645d4f0
jblair0039 <44126541+jblair0039@users.noreply.github.com>
2022-08-14 14:10:10 -0400

    Create windows-vm-main.tf

---
+++ windows-vm-main.tf
@@ -0,0 +1,50 @@
+resource "google_compute_instance" "vm_instance_public" {
+  name         = "jordandev-vm"
+  machine_type = var.windows_instance_type
+  zone         = var.gcp_zone
+  hostname     = "jordandev-vm"
+  tags         = ["rdp","http"]
+  boot_disk {
+    initialize_params {
+      image = var.windows_2022_sku
+    }
+  }
+  metadata = {
+    windows-startup-script-ps1 = <<-EOF
+      "#Start listening on a port 
+$Listener = [System.Net.Sockets.TcpListener]80;
+$Listener.Start();
+#wait, try connect from another PC etc."
+    EOF
+  }
+  network_interface {
+    network       = google_compute_network.vpc.name
+    subnetwork    = google_compute_subnetwork.network_subnet.name
+    access_config { }
+  }
+}
+resource "google_compute_instance" "vm_instance_public2" {
+  name         = "jordandev2-vm"
+  machine_type = var.windows_instance_type
+  zone         = var.gcp_zone
+  hostname     = "jordandev2-vm"
+  tags         = ["rdp","http"]
+  boot_disk {
+    initialize_params {
+      image = var.windows_2022_sku
+    }
+  }
+  metadata = {
+    windows-startup-script-ps1 = <<-EOF
+      "#Start listening on a port 
+$Listener = [System.Net.Sockets.TcpListener]80;
+$Listener.Start();
+#wait, try connect from another PC etc."
+    EOF
+  }
+  network_interface {
+    network       = google_compute_network.vpc.name
+    subnetwork    = google_compute_subnetwork.network_subnet.name
+    access_config { }
+  }
+}
