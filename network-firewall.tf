540f4ccb8e7b3451b6b057631cd304652862a4a7
jblair0039 <44126541+jblair0039@users.noreply.github.com>
2022-08-14 14:34:38 -0400

    Update network-firewall.tf

--- network-firewall.tf
+++ network-firewall.tf
@@ -21,3 +21,13 @@ resource "google_compute_firewall" "allow-rdp" {
   source_ranges = ["0.0.0.0/0"]
   target_tags = ["rdp"]
 }
+# allow icmp
+resource "google_compute_firewall" "allow-icmp" {
+  name    = "jordandev-fw-allow-icmp"
+  network = google_compute_network.vpc.name
+  allow {
+    protocol = "icmp"
+  }
+  source_ranges = ["0.0.0.0/0"]
+  target_tags = ["icmp"]
+}
