00086efe54aa2cbc6dc91c41e092ac7f2a71cef1
jblair0039 <44126541+jblair0039@users.noreply.github.com>
2022-08-14 14:09:39 -0400

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,14 @@
+variable "gcp_region" {
+  type = string
+description = "GCP Region"
+}
+
+variable "gcp_zone" {
+  type = string
+  description = "GCP Zone"
+}
+
+variable "gcp_project" {
+  type = string
+  description = "GCP Project Name"
+}
