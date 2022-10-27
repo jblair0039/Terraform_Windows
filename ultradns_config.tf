4f5d5e88317234ddcf72f4cc5a03dcdc305dd44f
jblair0039 <44126541+jblair0039@users.noreply.github.com>
2022-10-27 18:05:16 -0400

    Create ultradns_config.tf
    ultradns config

---
+++ ultradns_config.tf
@@ -0,0 +1,14 @@
+resource "ultradns_record" "tftest" {
+    zone_name = "e-hpstest.com."
+    owner_name = "tftest"
+    record_type = "1"
+    ttl = 120
+    record_data = [google_compute_instance.vm_instance_public.network_interface[0].access_config.0.nat_ip]
+}
+resource "ultradns_record" "tftest2" {
+    zone_name = "e-hpstest.com."
+    owner_name = "tftest2"
+    record_type = "1"
+    ttl = 120
+    record_data = [google_compute_instance.vm_instance_public2.network_interface[0].access_config.0.nat_ip]
+}
