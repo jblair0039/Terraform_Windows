1b2dcb898f693a8276bc818c67cae64779d42db3
jblair0039 <44126541+jblair0039@users.noreply.github.com>
2024-11-16 01:23:52 -0500

    Update ultradns_config.tf

--- ultradns_config.tf
+++ ultradns_config.tf
@@ -1,12 +1,12 @@
 resource "ultradns_record" "tftest" {
-    zone_name = "e-hpstest.com."
+    zone_name = "abc.com."
     owner_name = "tftest"
     record_type = "1"
     ttl = 120
     record_data = [google_compute_instance.vm_instance_public.network_interface[0].access_config.0.nat_ip]
 }
 resource "ultradns_record" "tftest2" {
-    zone_name = "e-hpstest.com."
+    zone_name = "abc.com."
     owner_name = "tftest2"
     record_type = "1"
     ttl = 120
